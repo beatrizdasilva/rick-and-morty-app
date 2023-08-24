0ac09d71a0a7ec472d354f188f9c5ada5abcad81
Beatriz da Silva <beatriz.silva@picpay.com>
2023-08-24 09:18:16 -0300

    adjusts service layer

--- app-estudo/app-estudo/Source/Episodes/EpisodesService.swift
+++ app-estudo/app-estudo/Source/Episodes/EpisodesService.swift
@@ -2,7 +2,6 @@ import API
 
 protocol EpisodesServicing {
     func getEpisode(completion: @escaping(Result<EpisodesResult, ApiError>) -> Void)
-    func getEpisode(by page: Int, completion: @escaping(Result<EpisodesResult, ApiError>) -> Void)
     func getEpisodeBySearch(by name: String, page: Int, completion: @escaping(Result<EpisodesResult, ApiError>) -> Void)
 }
 
@@ -11,21 +10,8 @@ class EpisodesService: EpisodesServicing {
     
     func getEpisode(completion: @escaping (Result<EpisodesResult, API.ApiError>) -> Void) {
         api.cancel()
-        api.execute(endpoint: EpisodeEndpoints.episodes(page: nil)) { (result: Result<EpisodesResult, ApiError>) in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let value):
-                    completion(.success(value))
-                case .failure(let error):
-                    completion(.failure(error))
-                }
-            }
-        }
-    }
-    
-    func getEpisode(by page: Int, completion: @escaping (Result<EpisodesResult, API.ApiError>) -> Void) {
-        api.cancel()
-        api.execute(endpoint: EpisodeEndpoints.episodes(page: page)) { (result: Result<EpisodesResult, ApiError>) in
+        api.execute(endpoint: EpisodeEndpoints.episodes(page: nil)) {
+            (result: Result<EpisodesResult, ApiError>) in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let value):
@@ -39,7 +25,8 @@ class EpisodesService: EpisodesServicing {
     
     func getEpisodeBySearch(by name: String, page: Int, completion: @escaping (Result<EpisodesResult, API.ApiError>) -> Void) {
         api.cancel()
-        api.execute(endpoint: EpisodeEndpoints.searchEpisodes(name: name, page: page)) { (result: Result<EpisodesResult, ApiError>) in
+        api.execute(endpoint: EpisodeEndpoints.searchEpisodes(name: name, page: page)) {
+            (result: Result<EpisodesResult, ApiError>) in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let value):
